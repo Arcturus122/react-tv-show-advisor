c21c36ee49d27b007c7e4eeaf1c782ed2d30572a
Louis-Alexis Dubief <louisalexis.dubief@gmail.com>
2023-09-24 16:39:35 +0200

    adding complete application

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY_PARAM = "?api_key=9a4c0dc296c5654c9f98dd687f3986e1";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
